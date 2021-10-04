089648205a85a135d605b363fa3bc3f28700eba9
Praveen Kumar Singh <49147942+PraveenKSI@users.noreply.github.com>
2021-10-04 11:22:19 +0530

    Update qs1.js

--- 1Oct/qs1.js
+++ 1Oct/qs1.js
@@ -1,3 +1,5 @@
+//Q1. Given this array: `[3,62,234,7,23,74,23,76,92]`, Using arrow function, create an array of the numbers greater than `70`.
+
 let arr = [3,62,234,7,23,74,23,76,92]
 let arr2 = arr.filter((val)=>{
     if(val > 70){
