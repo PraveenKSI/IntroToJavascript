f7e3524b014721ed2542f81d8831d1bb3a335687
Praveen Kumar Singh <49147942+PraveenKSI@users.noreply.github.com>
2021-10-05 11:30:23 +0530

    Add files via upload

---
+++ 4Oct/js5/qs5Main.js
@@ -0,0 +1,3 @@
+import {calcArea }from "./qs5Util.js";
+let area = calcArea(7);
+console.log(area);
---
+++ 4Oct/js5/qs5Util.js
@@ -0,0 +1,5 @@
+const pi = 3.14;
+function calcArea(radius){
+    return pi*radius*radius;
+}
+export {calcArea};
